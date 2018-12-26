519e09b363b606477ff7c56373424d0f80cb6e03
ZomBMage <ZomBMage@users.noreply.github.com>
2018-12-26 22:32:40 +0000

    Add scrolling text

--- core.py
+++ core.py
@@ -4,11 +4,18 @@ import screen
 
 device = max7219.matrix()
 
-for i in range(10000):
-    display = screen.gen_screen_from_num(display, i)
-    commands = screen.output_to_screen(display)
-    for command_set in commands:
-        command_to_run = []
-        for command in command_set:
-            command_to_run.extend(command)
-        device._write(command_to_run)
+# for i in range(10000):
+#     display = screen.gen_screen_from_num(display, i)
+#     commands = screen.output_to_screen(display)
+#     for command_set in commands:
+#         command_to_run = []
+#         for command in command_set:
+#             command_to_run.extend(command)
+#         device._write(command_to_run)
+
+commands = screen.show_message(screen.screen, "Hello World!")
+for command_set in commands:
+    command_to_run = []
+    for command in command_set:
+        command_to_run.extend(command)
+    device._write(command_to_run)
--- screen.py
+++ screen.py
@@ -6,7 +6,7 @@ def printscr(screen):
 
 def break_screens(screen):
     matrices = []
-    for i in range(4):
+    for i in range(int(len(screen[0])/8)):
         temp_matrix = []
         for j in range(8):
             temp_matrix.append(screen[j][i*8:(i+1)*8])
@@ -24,6 +24,7 @@ def join_screens(matrices):
 
 def sub_char(screen, character, matrix):
     matrices = break_screens(screen)
+    character = [[int(i) for i in str(bin(j))[2:].zfill(8)] for j in character]
     matrices[matrix] = character
     screen = join_screens(matrices)
     return screen
@@ -62,3 +63,67 @@ def output_to_screen(screen):
             temp_commands.append([register,data])
         commands.append(temp_commands)
     return commands
+
+def show_message(screen, message):
+    message = "    "+message.upper()+"    "
+    scrolling_screen = [[0 for i in range(8*len(message)+8)] for j in range(8)]
+    for i in enumerate(message):
+        if i[1] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            scrolling_screen = sub_char(scrolling_screen, getattr(font.letters, "letter_"+i[1]), i[0])
+        elif i[1] in "0123456789":
+            scrolling_screen = sub_char(scrolling_screen, getattr(font.numbers, "digit_"+i[1]), i[0])
+        elif i[1] == "!":
+            scrolling_screen = sub_char(scrolling_screen, font.special.exclamation_mark, i[0])
+        elif i[1] == "\"":
+            scrolling_screen = sub_char(scrolling_screen, font.special.quotation_mark, i[0])
+        elif i[1] == "#":
+            scrolling_screen = sub_char(scrolling_screen, font.special.hashtag, i[0])
+        elif i[1] == "$":
+            scrolling_screen = sub_char(scrolling_screen, font.special.dollar_sign, i[0])
+        elif i[1] == "%":
+            scrolling_screen = sub_char(scrolling_screen, font.special.percent_sign, i[0])
+        elif i[1] == "&":
+            scrolling_screen = sub_char(scrolling_screen, font.special.ampersand, i[0])
+        elif i[1] == "'":
+            scrolling_screen = sub_char(scrolling_screen, font.special.apostraphe, i[0])
+        elif i[1] == "(":
+            scrolling_screen = sub_char(scrolling_screen, font.special.bracket_left, i[0])
+        elif i[1] == ")":
+            scrolling_screen = sub_char(scrolling_screen, font.special.bracket_right, i[0])
+        elif i[1] == "*":
+            scrolling_screen = sub_char(scrolling_screen, font.special.asterisk, i[0])
+        elif i[1] == "+":
+            scrolling_screen = sub_char(scrolling_screen, font.special.plus_sign, i[0])
+        elif i[1] == ",":
+            scrolling_screen = sub_char(scrolling_screen, font.special.comma, i[0])
+        elif i[1] == ".":
+            scrolling_screen = sub_char(scrolling_screen, font.special.full_stop, i[0])
+        elif i[1] == "-":
+            scrolling_screen = sub_char(scrolling_screen, font.special.hyphen, i[0])
+        elif i[1] == "/":
+            scrolling_screen = sub_char(scrolling_screen, font.special.slash, i[0])
+        elif i[1] == ":":
+            scrolling_screen = sub_char(scrolling_screen, font.special.colon, i[0])
+        elif i[1] == ";":
+            scrolling_screen = sub_char(scrolling_screen, font.special.semicolon, i[0])
+        elif i[1] == "<":
+            scrolling_screen = sub_char(scrolling_screen, font.special.less_than_sign, i[0])
+        elif i[1] == ">":
+            scrolling_screen = sub_char(scrolling_screen, font.special.greater_than_sign, i[0])
+        elif i[1] == "=":
+            scrolling_screen = sub_char(scrolling_screen, font.special.equal_sign, i[0])
+        elif i[1] == "?":
+            scrolling_screen = sub_char(scrolling_screen, font.special.question_mark, i[0])
+        else:
+            scrolling_screen = sub_char(scrolling_screen, font.patterns.empty, i[0])
+    
+    command_list = []
+    for i in range(len(scrolling_screen[0])-32):
+        screen = [scrolling_screen[row][i:i+32] for row in range(8)]
+        print(screen)
+        command_list.extend(output_to_screen(screen))
+    
+    return command_list
+
+
+show_message(screen, "Hello world!")
